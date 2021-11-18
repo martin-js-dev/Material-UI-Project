43c763f1ca88e837f8b092ec14bf9933190daeb1
Your Name <you@example.com>
2021-11-18 19:30:53 +0100

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
-
+import Image from '@material-ui/core/Image'
 
 
 const App = () => {
@@ -12,7 +12,10 @@ const App = () => {
         <div className="App">
             <Typography variant="h4">Welcome to my Material UI site</Typography>
             <Button variant="contained" color="primary">Hello</Button>
-            
+            <Image ><img
+        src={`${burger.png}?w=164&h=164&fit=crop&auto=format`}
+     
+      /></Image>
         </div>
         </Container>
      );
