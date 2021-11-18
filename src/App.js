1256cc18df9add7fd6b86f218a60155cb4ef92e2
Your Name <you@example.com>
2021-11-18 19:17:17 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+
+
+
+const App = () => {
+    return ( 
+        <Container>
+        <div className="App">
+            <Typography variant="h4">Welcome to my Material UI site</Typography>
+            <Button variant="contained" color="primary">Hello</Button>
+            
+        </div>
+        </Container>
+     );
 }
-
+ 
 export default App;
+
